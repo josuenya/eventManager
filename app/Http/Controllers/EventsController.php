ed31d42e85c44e0b533c982169dfacfee5ac8f7b
josuenya <josuenya23@gmail.com>
2022-08-22 00:29:24 +0100

    wip

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -91,10 +91,10 @@ class EventsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
+//     public function show($id)
+//     {
+//         //
+//     }
 
     /**
      * Show the form for editing the specified resource.
